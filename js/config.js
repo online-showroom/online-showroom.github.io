1ecafc546efe35a2a0ddcd7eaf7d1086705957ef
hopeful2 <hopeful2@goandmake.app>
2019-02-21 02:08:13 +1100

    draft homepage

---
+++ js/config.js
@@ -0,0 +1,16 @@
+window.gtpb = window.gtpb || {};
+
+
+
+     
+
+     
+
+
+gtpb.default.site_id = "gt";
+gtpb.default.SiteMenu = {"global":[{"URL":"/tutorial/","Page":null,"Name":"Tutorials","Menu":"global","Identifier":"tutorials","Pre":"","Post":"","Weight":5,"Parent":"","Children":null},{"URL":"/faq/","Page":null,"Name":"FAQ","Menu":"global","Identifier":"","Pre":"","Post":"","Weight":12,"Parent":"","Children":null},{"URL":"/about/","Page":null,"Name":"About","Menu":"global","Identifier":"about","Pre":"","Post":"","Weight":15,"Parent":"","Children":null}],"iconmenu":[{"URL":"#","Page":null,"Name":"Go Account","Menu":"iconmenu","Identifier":"","Pre":"Go","Post":"","Weight":0,"Parent":"","Children":null}]};
+gtpb.default.SiteParams = {"absolutebuilderurl":true,"accent_color":"hsl(165,35%,45%)","accent_color_light":"hsl(165,35%,65%)","css_url":"https://fonts.googleapis.com/css?family=Gloria+Hallelujah","custom_css":".handwriting {\n font-family: 'Gloria Hallelujah', cursive;\n}\n\n\n@keyframes slowRightEntry {\n  0% {\n    transform: translateX(-100vw);\n    opacity: 0.3;\n  }\n50% {\n   opacity: .5;\n}\n\n  90% {\n   transform: translateX(0vw);\n   opacity: .8;  \n   }\n  100% {\n    transform: translateX(0vw);\n   opacity: 0.7;\n}\n}\n\n.slowRightEntry {\n  animation: slowRightEntry 10s linear 10;\n  height: 100%;\n  }\n\n\n@keyframes slowRightExit {\n  0% {\n    transform: translateX(0vw);\n   opacity: 1;\n  }\n\n  50% {\n    opacity: .1;\n }\n  100% {\n   opacity: .0;\n    transform: translateX(50vw);\n  }\n}\n\n.slowRightExit {\n  animation: slowRightExit 20s linear 5;\n  height: 100%;\n}\n","flex_box_interior_classes":"w-100 w-50-m w-33-l ","global_section":{},"header_class":"bg-primary-color","js_export":{},"js_partial":{},"lightness":45,"lightness_dark":20,"lightness_light":65,"lightness_lighter":80,"logo_url":"/images/logos/icon-on-dark.svg","mainSections":["post"],"main_menu_align":"start","mainsections":["post"],"mobile_menu_button":[{"label":"Menu","menu":"global"},{"label":"Account","menu":"iconmenu"}],"primary_hue":345,"saturation":35,"skip_cover_nav":false};
+gtpb.default.SiteBaseURL = "http://builder.goandmake.app";
+gtpb.default.SiteCopyright = "Copyright 2019, GoAndMake.app.";
+gtpb.default.SiteTitle = "Go and talk page builder";
+gtpb.default.SiteFooter = {"appearance":{"legal_link_color":"link-white-80","nav_link_color":"link-gold","section_background":"bg-black-80"},"is_global":true,"menu":{"footer_legal":[{"name":"Terms of Service","url":"#"},{"name":"Privacy","url":"#"}],"footer_nav":[{"name":"Home","url":"#"},{"name":"About","url":"#"}]}};
